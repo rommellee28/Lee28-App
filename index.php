2fed956b6fc4c1ed23062b79751c7cb94960a574
Rommellee28 <rommellee28@gmail.com>
2023-05-03 17:48:04 -0400

    Inicio de la app - Layout

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My App 1.0</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+	<div class="container">
+
+<!-- Panel Lateral -->
+		<div class="panel">
+			<div class="panel-header">
+				<h2>Menu</h2>
+			</div>
+			<div class="panel-content">
+				<ul>
+					<a href="">
+						<li class="panel-item">Home</li>
+					</a>
+				</ul>
+			</div>
+		</div>
+
+<!-- Aplicación -->
+		<div class="app">
+			<div class="app-header">
+				<div class="toggle-panel">
+					<button></button>
+				</div>
+				<div class="search-bar">
+					<input type="text" name="">
+				</div>
+				<div class="user-menu">
+					<div class="user-name">
+						Default Username
+					</div>
+					<div class="user-img">
+						<img src="">
+					</div>
+				</div>
+			</div>
+			<div class="app-content">
+				<h1>Hola Mundo</h1>
+			</div>
+		</div>
+	</div>
+
+
+<!-- Scripts -->
+	<script src="./js/scripts.js"></script>
+</body>
+</html>
